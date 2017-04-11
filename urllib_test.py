e25471ab48ea1cd6fa088207969dadfa464f2072
ad <ad@itmp.top>
2017-04-11 17:34:08 +0800

    cookie实例

--- urllib_test.py
+++ urllib_test.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
-# -*- coding:UTF-8 -*-
+#-*- coding:UTF-8 -*-
 
-import cookielib
+import urllib
 import  urllib2
+import cookielib
 
-cookie = cookielib.MozillaCookieJar()
-cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
-req = urllib2.Request("http://www.baidu.com")
+filename = 'cookie.txt'
+cookie = cookielib.MozillaCookieJar(filename)
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-response = opener.open(req)
-
-print response.read()
+postdata = urllib.urlencode({'stupid':'201200131012','pwd':'23342321'})
+loginUrl = 'http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bks_login2.login'
+result = opener.open(loginUrl,postdata)
+cookie.save(ignore_discard=True, ignore_expires=True)
+gradeUrl = 'http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bkscjcx.curscopre'
+result = opener.open(gradeUrl)
+print result.read()
