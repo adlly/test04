398e5140ea6a6e766c3015c1f19ff56bf17dbe41
ad <ad@itmp.top>
2017-04-11 16:41:35 +0800

    保存cookie文件

--- urllib_test.py
+++ urllib_test.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
+import cookielib
 import urllib2
-import  cookielib
 
-cookie = cookielib.CookieJar()
-
+filename = 'cookie.txt'
+cookie = cookielib.MozillaCookieJar(filename)
 handler = urllib2.HTTPCookieProcessor(cookie)
 opener = urllib2.build_opener(handler)
-response = opener.open('http://www.baidu.com')
-for item in cookie:
-    print 'Name = '+item.name
-    print 'Value = '+item.value
+
+response = opener.open("http://www.baidu.com")
+cookie.save(ignore_discard=True, ignore_expires=True)
