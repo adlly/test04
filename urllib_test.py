cc0de028da80eaedc2740ffa52307b2f72c755bd
ad <ad@itmp.top>
2017-04-11 14:40:51 +0800

    url error

--- urllib_test.py
+++ urllib_test.py
@@ -2,8 +2,9 @@
 # -*- coding:UTF-8 -*-
 
 import urllib2
-httpHandler = urllib2.HTTPHandler(debuglevel=1)
-httpsHandler = urllib2.HTTPSHandler(debuglevel=1)
-opener = urllib2.build_opener(httpHandler,httpHandler)
-urllib2.install_opener(opener)
-response = urllib2.urlopen("http://www.baidu.com")
+
+request = urllib2.Request('http://www.xxxxx.com')
+try:
+    urllib2.urlopen(request)
+except urllib2.URLError, e:
+    print e.reason
