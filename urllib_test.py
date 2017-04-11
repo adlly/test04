3b3e7534141058e4fd32cf95c40d08e7656b0878
ad <ad@itmp.top>
2017-04-11 11:13:25 +0800

    put & delete

--- urllib_test.py
+++ urllib_test.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
-
+import urllib
 import urllib2
-
-enable_proxy = True
-proxy_handler = urllib2.ProxyHandler({"http":'http://some-proxy.com:8080'})
-null_proxy_handler = urllib2.ProxyHandler({})
-if enable_proxy:
-    opener = urllib2.build_opener(proxy_handler)
-else:
-    opener = urllib2.build_opener(null_proxy_handler)
-urllib2.install_opener(opener)
+import  httplib
+request = urllib2.Request("http://www.baidu.com", data="baidu")
+request.get_method = lambda: 'PUT' # or 'DELETE'
+response = urllib2.urlopen(request)
