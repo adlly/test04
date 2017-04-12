17798205b427366344d09561bed1742ce7834771
ad <ad@itmp.top>
2017-04-12 15:19:36 +0800

    正则2

--- urllib_test.py
+++ urllib_test.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python
 # -*- coding:UTF-8 -*-
 
-__author__ = 'addy'
-
 import re
 
-pattern = re.compile(r'hello')
+m = re.match(r'(\w+) (\w+)(?P<sign>,*)','hello world!')
 
-result1 = re.match(pattern,'hello')
-result2 = re.match(pattern,'helloo CQC!')
-result3 = re.match(pattern,'helo CQC!')
-result4 = re.match(pattern,'hello CQC!')
-
-if result1:
-    print result1.group()
-else:
-    print '1 does not match'
-
-if result2:
-    print result2.group()
-else:
-    print '2 dose not match'
-
-
-if result3:
-    print result3.group()
-else:
-    print '3 dose not match'
-
-if result4:
-    print result4.group()
-else:
-    print '4 dose not match'
+print "m.string:",m.string
+print "m.re:",m.re
+print "m.pos:",m.pos
+print "m.endpos:",m.endpos
+print "m.lastindex:",m.lastindex
+print "m.lastgroup:",m.lastgroup
+print "m.group():",m.group()
+print "m.group(1,2):",m.group(1,2)
+print "m.groups():",m.groups()
+print "m.groupdict()",m.groupdict()
+print "m.start(2):",m.start(2)
+print "m.end(2):",m.end(2)
+print "m.span(2):",m.span(2)
+print r"m.expand(r'\g \g\g'):", m.expand(r'\2 \1\3')
