4af008533a461e5d70d858e91a666b619f954964
ad <ad@itmp.top>
2017-04-12 16:35:13 +0800

    正则 end

--- urllib_test.py
+++ urllib_test.py
@@ -3,9 +3,32 @@
 
 import re
 
-pattern = re.compile(r'world')
+# pattern = re.compile(r'\d+')
+# print re.split(pattern,'one1two2three3four4')
 
-match = re.search(pattern,'hello world!')
+# pattern = re.compile(r'\d+')
+# print re.findall(pattern,'one1two2three3four4')
 
-if match:
-    print match.group()
+# pattern = re.compile(r'\d+')
+# for m in re.finditer(pattern,'one1two2three3four4'):
+#     print m.group()
+
+# pattern = re.compile(r'(\w+) (\w+)')
+# s = 'i say, hello world!'
+# print re.sub(pattern,r'\2 \1',s)
+#
+# def func(m):
+#     return m.group(1).title() + ' ' + m.group(2).title()
+#
+# print re.sub(pattern,func,s)
+
+
+pattern = re.compile(r'(\w+) (\w+)')
+s = 'i say, hello world!'
+
+print re.subn(pattern, r'\2 \1', s)
+
+def func(m):
+    return m.group(1).title() + ' ' + m.group(2).title()
+
+print re.subn(pattern,func,s)
