5ad370739a08b4cc805caf7cb8f8c42cc236b39f
ad <ad@itmp.top>
2017-04-12 14:34:55 +0800

    url error correct

--- urllib_test.py
+++ urllib_test.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python
 #-*- coding:UTF-8 -*-
 
-import urllib
-import  urllib2
-import cookielib
+import urllib2
+req = urllib2.Request('http://xx.com')
 
-filename = 'cookie.txt'
-cookie = cookielib.MozillaCookieJar(filename)
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-postdata = urllib.urlencode({'stupid':'201200131012','pwd':'23342321'})
-loginUrl = 'http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bks_login2.login'
-result = opener.open(loginUrl,postdata)
-cookie.save(ignore_discard=True, ignore_expires=True)
-gradeUrl = 'http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bkscjcx.curscopre'
-result = opener.open(gradeUrl)
-print result.read()
+try :
+    resp = urllib2.urlopen(req)
+    resp = urllib2.urlopen(req)
+except urllib2.URLError as e:
+  if hasattr(e, 'reason'):
+    print(e.reason)
+    print e
+  else:
+    print('error')
