5c2392e32394e6898181fc935673372a5882012f
ad <ad@itmp.top>
2017-04-13 17:58:20 +0800

    证书

--- crawl_qiushi.py
+++ crawl_qiushi.py
@@ -3,6 +3,8 @@
 
 import urllib
 import urllib2
+import re
+import bs4
 
 url = "http://www.qiushibaike.com/hot/page/1"
 user_agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36"
@@ -10,7 +12,15 @@ headers = {'User-Agent': user_agent}
 try :
     request = urllib2.Request(url, headers= headers)
     response =  urllib2.urlopen(request)
-    print response.read()
+    content =  response.read().decode('utf-8')
+    # pattern = re.compile('<div.*?author">.*?<a.*?<img.*?>(.*?)</a>.*?<div.*?'+
+    #                      'content">(.*?)<!--(.*?)-->.*?</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
+    items = bs4.findall(content)
+    print items
+    # for item in items:
+    #
+    #   print item[0],item[1],item[2],item[3],item[4]
+
 except urllib2.URLError,e:
     if hasattr(e,"code"):
         print e.code
