cefbf3ac6c1ba7af4cea08fbad1cfdd0d0db2576
ad <ad@itmp.top>
2017-04-13 14:35:51 +0800

    requests.post()

--- request.py
+++ request.py
@@ -1,8 +1,6 @@
-import urllib2
 import requests
 
-payload = {'key1': 'value1','key2':'value2'}
+payloads = {'key1': 'value1', 'key2': 'value2'}
+r = requests.post("http://httpbin.org/post", data = payloads)
 
-r = requests.get("http://httpbin.org/get", params=payload)
-print r.url
-
+print r.text
